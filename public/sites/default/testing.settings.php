e88d17318a06bb246cbf05937186c43ed6bdb94d
tuutti <tuutti@iki.fi>
2021-08-31 12:48:14 +0300

    UHF-2060: Cron configurations, proxy configurations, blob storage

---
+++ public/sites/default/testing.settings.php
@@ -0,0 +1,8 @@
+<?php
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'test-administration',
+  'fi' => 'test-kaupunki-ja-hallinto',
+  'sv' => 'test-stad-och-forvaltning',
+  'ru' => 'test-administration',
+];
+
