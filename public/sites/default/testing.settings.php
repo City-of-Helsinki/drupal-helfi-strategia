e1aa2cc33831bfaf2ad85b658dfe0cd40e2026e0
tuutti <tuutti@iki.fi>
2021-09-30 12:47:30 +0300

    Updated new proxy path

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'test-administration',
+  'en' => 'test-decision-making',
   'fi' => 'test-paatoksenteko-ja-hallinto',
   'sv' => 'test-beslutsfattande-och-forvaltning',
   'ru' => 'test-administration',
