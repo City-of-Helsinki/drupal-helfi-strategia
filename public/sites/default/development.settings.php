07498eba989b0fad5dcae0e6cbd88e9b36b477d0
tuutti <tuutti@iki.fi>
2022-05-10 08:38:11 +0300

    Tunnistamo return URLs

--- public/sites/default/development.settings.php
+++ public/sites/default/development.settings.php
@@ -1,7 +1,7 @@
 <?php
 
+$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/dev-paatoksenteko-ja-hallinto/openid-connect/tunnistamo';
 $config['helfi_proxy.settings']['asset_path'] = 'dev-hallinto-assets';
-
 $config['helfi_proxy.settings']['prefixes'] = [
   'en' => 'dev-decision-making',
   'fi' => 'dev-paatoksenteko-ja-hallinto',
