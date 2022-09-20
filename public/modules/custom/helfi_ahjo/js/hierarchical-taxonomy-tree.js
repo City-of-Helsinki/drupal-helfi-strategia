989062199cb0d750dafbf7a1de93e18e293f683c
Arsene Claudiu Ion <arseneclaudiuion@192-168-0-179.rdsnet.ro>
2022-09-20 09:40:22 +0300

    reverted js file to fix a bug

--- public/modules/custom/helfi_ahjo/js/hierarchical-taxonomy-tree.js
+++ public/modules/custom/helfi_ahjo/js/hierarchical-taxonomy-tree.js
@@ -2,37 +2,35 @@
  * @file
  */
 
-(function ($, Drupal, drupalSettings, once) {
+(function ($, Drupal, drupalSettings) {
   'use strict';
-  Drupal.behaviors.hierarchicalTaxonomyTree = {
-    attach: function (context, settings) {
-      once('hierarchicalTaxonomyTree', 'html', context).forEach( function (element) {
-        $('.menu-item.menu-item--expanded').each(function (i, obj) {
-          let self = $(this);
-          if (self.find('a.active').length) {
-            self.addClass('active');
+  $(document).ready(function () {
+    $('.menu-item.menu-item--expanded').each(function (i, obj) {
+      let self = $(this);
+      if (self.find('a.active').length) {
+        self.addClass('active');
 
-            if (drupalSettings.interactiveParentMenu) {
-              if (!self.hasClass('menu-item--active')) {
-                self.children('i').toggleClass('arrow-right arrow-down');
-              }
-            }
+        if (drupalSettings.interactiveParentMenu) {
+          if (!self.hasClass('menu-item--active')) {
+            self.children('i').toggleClass('arrow-right arrow-down');
           }
-        });
+        }
+      }
+    });
 
-        $('.hierarchical-taxonomy-tree .menu-item--expanded > a').on('click', function (e) {
-          e.preventDefault();
-          $(this).find('i').toggleClass('arrow-right arrow-down');
-          let isChildVisible = $(this).parent().children('.menu').is(':visible');
-          if (isChildVisible) {
-            $(this).parent().children('.menu').slideUp();
-            $(this).parent().removeClass('active');
-          } else {
-            $(this).parent().children('.menu').slideDown();
-            $(this).parent().addClass('active');
-          }
-        });
-      });
-    }
-  }
-} (jQuery, Drupal, drupalSettings, once));
+    $('.hierarchical-taxonomy-tree .menu-item--expanded > a').on('click', function (e) {
+      e.preventDefault();
+      $(this).find('i').toggleClass('arrow-right arrow-down');
+      let isChildVisible = $(this).parent().children('.menu').is(':visible');
+      if (isChildVisible) {
+        $(this).parent().children('.menu').slideUp();
+        $(this).parent().removeClass('active');
+      }
+      else {
+        $(this).parent().children('.menu').slideDown();
+        $(this).parent().addClass('active');
+      }
+    });
+
+  });
+})(jQuery, Drupal, drupalSettings);
