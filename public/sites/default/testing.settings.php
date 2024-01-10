507d3bf32864b174e7e252a57831f69758f81862
tuutti <tuutti@iki.fi>
2024-01-10 07:59:27 +0200

    UHF-9517: Fixed asset path

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,4 +1,3 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/test-paatoksenteko-ja-hallinto/openid-connect/tunnistamo';
-$config['helfi_proxy.settings']['asset_path'] = 'test-hallinto-assets';
