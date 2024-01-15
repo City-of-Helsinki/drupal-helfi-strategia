c2188561facd99bd5c1a4654c6869f16e477d74b
tuutti <tuutti@iki.fi>
2024-01-15 07:17:53 +0200

    UHF-9517: Use env specific subdomain instead of paths

--- public/sites/default/staging.settings.php
+++ public/sites/default/staging.settings.php
@@ -1,12 +1,1 @@
 <?php
-
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'staging-decision-making',
-  'fi' => 'staging-paatoksenteko-ja-hallinto',
-  'sv' => 'staging-beslutsfattande-och-forvaltning',
-  'ru' => 'staging-administration',
-  'zxx' => 'staging-decision-making',
-];
-$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/staging-paatoksenteko-ja-hallinto/openid-connect/tunnistamo';
-
-$config['helfi_proxy.settings']['asset_path'] = 'staging-hallinto-assets';
--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,3 +1,1 @@
 <?php
-
-$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/test-paatoksenteko-ja-hallinto/openid-connect/tunnistamo';
