334248f786485758f828477f91a5ca4c9fe62fd4
Anna Lonka <anna.lonka@druid.fi>
2025-11-28 17:45:33 +0200

    UHF-12571: Created hero  block for hyte search

---
+++ public/modules/custom/helfi_strategia/src/Plugin/Block/HyteSearchHeroBlock.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_strategia\Plugin\Block;
+
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'HyteSearchHeroBlock' block.
+ */
+#[Block(
+  id: "hyte_search_hero_block",
+  admin_label: new TranslatableMarkup("Hyte search hero block"),
+)]
+final class HyteSearchHeroBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route match.
+   */
+  private RouteMatchInterface $routeMatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) : self {
+    $instance = new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition
+    );
+
+    $instance->routeMatch = $container->get(RouteMatchInterface::class);
+
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() : array {
+    $route = $this->routeMatch->getRouteName();
+
+    // Routes where the hero should appear.
+    $routes = [
+      'helfi_strategia.hyte_search',
+    ];
+
+    if (!in_array($route, $routes, TRUE)) {
+      return [];
+    }
+
+    $title = new TranslatableMarkup($this->routeMatch->getRouteObject()->getDefault('_title'), [], ['context' => 'Hyte search']);
+    $description = '';
+
+    return $this->buildHero($title, $description);
+  }
+
+  /**
+   * Builds the hero block render array.
+   */
+  private function buildHero(
+    TranslatableMarkup $title,
+    ?string $description,
+  ) : array {
+
+    return [
+      '#theme' => 'hyte_search_hero_block',
+      '#hero_title' => $title,
+      '#hero_description' => $description,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() : array {
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route']);
+  }
+}
