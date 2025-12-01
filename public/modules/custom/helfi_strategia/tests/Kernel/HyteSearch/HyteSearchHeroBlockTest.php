d2361872364a8f4269cc3969ba2a33bb50610731
Anna Lonka <anna.lonka@druid.fi>
2025-12-01 17:23:30 +0200

    UHF-12571: Removed route match and cache context

--- public/modules/custom/helfi_strategia/tests/Kernel/HyteSearch/HyteSearchHeroBlockTest.php
+++ public/modules/custom/helfi_strategia/tests/Kernel/HyteSearch/HyteSearchHeroBlockTest.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\helfi_strategia\Kernel;
 
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\helfi_strategia\Plugin\Block\HyteSearchHeroBlock;
 use Symfony\Component\DependencyInjection\Loader\Configurator\Traits\PropertyTrait;
-use Symfony\Component\Routing\Route;
 
 /**
  * Kernel tests for HyteSearchHeroBlock.
@@ -46,21 +44,6 @@ class HyteSearchHeroBlockTest extends KernelTestBase {
   public function testBuildMethod(): void {
     $plugin_definition = ['provider' => 'helfi_strategia'];
 
-    // Create a real Route object with a _title.
-    $route = new Route(
-      path: '/find-services',
-      defaults: ['_title' => 'Find wellbeing services']
-    );
-
-    // Mock RouteMatchInterface.
-    $routeMatch = $this->prophesize(RouteMatchInterface::class);
-    $routeMatch->getRouteName()
-      ->willReturn('helfi_strategia.hyte_search');
-    $routeMatch->getRouteObject()
-      ->willReturn($route);
-
-    // Register mock in the container.
-    $this->container->set(RouteMatchInterface::class, $routeMatch->reveal());
     $block = HyteSearchHeroBlock::create($this->container, [], 'hyte_search_hero_block', $plugin_definition);
 
     $build = $block->build();
@@ -72,24 +55,6 @@ class HyteSearchHeroBlockTest extends KernelTestBase {
     $this->assertArrayHasKey('#theme', $content);
     $this->assertEquals('hyte_search_hero_block', $content['#theme']);
     $this->assertEquals('Find wellbeing services', (string) $content['#hero_title']);
-  }
-
-  /**
-   * Tests cache contexts.
-   */
-  public function testCacheContexts(): void {
-    $plugin_definition = ['provider' => 'helfi_strategia'];
-
-    $block = HyteSearchHeroBlock::create(
-      $this->container,
-      [],
-      'hyte_search_hero_block',
-      $plugin_definition
-    );
-
-    $contexts = $block->getCacheContexts();
-
-    $this->assertContains('route', $contexts);
   }
 
 }
