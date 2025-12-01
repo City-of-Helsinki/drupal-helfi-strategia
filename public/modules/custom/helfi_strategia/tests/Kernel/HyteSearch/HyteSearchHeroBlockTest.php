87712532b8c5ee0e011485b18bc4d9b66971f51a
Anna Lonka <anna.lonka@druid.fi>
2025-12-01 08:42:28 +0200

    UHF-12571: Fix tests

--- public/modules/custom/helfi_strategia/tests/Kernel/HyteSearch/HyteSearchHeroBlockTest.php
+++ public/modules/custom/helfi_strategia/tests/Kernel/HyteSearch/HyteSearchHeroBlockTest.php
@@ -24,6 +24,7 @@ class HyteSearchHeroBlockTest extends KernelTestBase {
     'block',
     'big_pipe',
     'helfi_strategia',
+    'helfi_api_base',
   ];
 
   /**
@@ -39,54 +40,32 @@ class HyteSearchHeroBlockTest extends KernelTestBase {
    * Tests the build() method of the hero block.
    */
   public function testBuildMethod(): void {
-    $plugin_definition = [
-      'provider' => 'helfi_strategia',
-    ];
+    $plugin_definition = ['provider' => 'helfi_strategia'];
 
+    // Mock RouteMatchInterface.
     $routeMatch = $this->prophesize(RouteMatchInterface::class);
-    $routeMatch->getRouteName()->willReturn('helfi_strategia.hyte_search.fi');
+    $routeMatch->getRouteName()->willReturn('helfi_strategia.hyte_search');
     $this->container->set(RouteMatchInterface::class, $routeMatch->reveal());
-
-    /** @var \Drupal\helfi_strategia\Plugin\Block\HyteSearchHeroBlock $block */
-    $block = HyteSearchHeroBlock::create(
-      $this->container,
-      [],
-      'hyte_search_hero_block',
-      $plugin_definition
+    $block = HyteSearchHeroBlock::create($this->container, [], 'hyte_search_hero_block', $plugin_definition
     );
 
     $build = $block->build();
 
-    // Structure checks.
     $this->assertIsArray($build);
     $this->assertArrayHasKey('hyte_search_hero_block', $build);
 
     $content = $build['hyte_search_hero_block'];
-
-    // Assert theme hook.
     $this->assertArrayHasKey('#theme', $content);
     $this->assertEquals('hyte_search_hero_block', $content['#theme']);
-    // Assert expected variables exist.
-    $this->assertArrayHasKey('#hero_title', $content);
-    $this->assertArrayHasKey('#hero_description', $content);
-
-    // Assert expected values.
-    $this->assertEquals('Your hero title', (string) $content['#hero_title']);
-    $this->assertEquals(
-      'Your hero description here.',
-      (string) $content['#hero_description']
-    );
+    $this->assertEquals('Find wellbeing services', (string) $content['#hero_title']);
   }
 
   /**
    * Tests cache contexts.
    */
   public function testCacheContexts(): void {
-    $plugin_definition = [
-      'provider' => 'helfi_strategia',
-    ];
+    $plugin_definition = ['provider' => 'helfi_strategia'];
 
-    /** @var \Drupal\helfi_strategia\Plugin\Block\HyteSearchHeroBlock $block */
     $block = HyteSearchHeroBlock::create(
       $this->container,
       [],
@@ -98,5 +77,4 @@ class HyteSearchHeroBlockTest extends KernelTestBase {
 
     $this->assertContains('route', $contexts);
   }
-
 }
