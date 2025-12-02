eef12ce716238db5bac87b8a2a863ba18cd7a895
Anna Lonka <anna.lonka@druid.fi>
2025-12-02 08:32:13 +0200

    UHF-12571: Removed route match and cache context

--- public/modules/custom/helfi_strategia/src/Plugin/Block/HyteSearchHeroBlock.php
+++ public/modules/custom/helfi_strategia/src/Plugin/Block/HyteSearchHeroBlock.php
@@ -7,9 +7,7 @@ namespace Drupal\helfi_strategia\Plugin\Block;
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'HyteSearchHeroBlock' block.
@@ -19,29 +17,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
   admin_label: new TranslatableMarkup("Hyte search hero block"),
 )]
 final class HyteSearchHeroBlock extends BlockBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The route match.
-   */
-  private RouteMatchInterface $routeMatch;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(
-    ContainerInterface $container,
-    $configuration,
-    $plugin_id,
-    $plugin_definition,
-  ) : self {
-    $instance = new self(
-      $configuration,
-      $plugin_id,
-      $plugin_definition
-    );
-
-    return $instance;
-  }
 
   /**
    * {@inheritdoc}
